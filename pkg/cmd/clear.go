5b588f95226b7dcae61f8c79a0a1c16f87fc3b0a
Ahmet OZER <git@ahmetozer.org>
2024-06-30 15:08:17 +0000

    feat: delete all container option

--- pkg/cmd/clear.go
+++ pkg/cmd/clear.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/ahmetozer/sandal/pkg/config"
@@ -8,13 +10,35 @@ import (
 )
 
 func clear(args []string) error {
+
+	f := flag.NewFlagSet("exec", flag.ExitOnError)
+
+	var (
+		help      bool
+		deleteAll bool
+	)
+
+	f.BoolVar(&help, "help", false, "show this help message")
+	f.BoolVar(&deleteAll, "all", false, "delete all containers which they are not in running state")
+
+	if err := f.Parse(args); err != nil {
+		return fmt.Errorf("error parsing flags: %v", err)
+	}
+
+	if help {
+		f.Usage()
+		return nil
+	}
+
 	conts, _ := config.AllContainers()
 	for _, c := range conts {
-		if !c.Remove {
-			continue
+		if !deleteAll {
+			if !c.Remove {
+				continue
+			}
 		}
 		if container.IsRunning(&c) {
-			log.Printf("container %s is running, %v", c.Name, c.Remove)
+			log.Printf("container %s is running,  rm=%v", c.Name, c.Remove)
 			continue
 		}
 		deRunContainer(&c)
