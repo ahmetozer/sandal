1de3c0e2215b8590fb86bf4b2df6059b809e4449
Ahmet OZER <git@ahmetozer.org>
2024-09-01 19:29:48 +0000

    feat: encapsulating sub commands with double quotes for

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -19,6 +19,14 @@ func cmd(args []string) error {
 	for _, c := range conts {
 		if c.Name == args[0] {
 			c.HostArgs[0] = os.Args[0] // sync with current command
+			for i := range c.HostArgs {
+				k := strings.Split(c.HostArgs[i], "=")
+				if len(k) > 1 {
+					k[1] = "\"" + k[1]
+					k[len(k)-1] = k[len(k)-1] + "\""
+					c.HostArgs[i] = strings.Join(k, "=")
+				}
+			}
 			fmt.Println(strings.Join(c.HostArgs, " "))
 			return nil
 		}
