e1c60425fea55e86c50de7ed0d28260694b22ed8
Ahmet OZER <10574489+ahmetozer@users.noreply.github.com>
2026-03-24 17:33:22 +0000

    poweroff to not hang cpu

--- main_linux.go
+++ main_linux.go
@@ -8,15 +8,19 @@ import (
 
 	"github.com/ahmetozer/sandal/pkg/cmd"
 	"github.com/ahmetozer/sandal/pkg/container/cruntime"
+	"golang.org/x/sys/unix"
 )
 
 func platformMain() {
 	if cruntime.IsVMInit() {
 		if err := cruntime.VMInit(); err != nil {
 			fmt.Fprintf(os.Stderr, "VMInit error: %v\n", err)
+			unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)
+			os.Exit(1)
 		}
-		// PID 1 must not exit; power off the VM before os.Exit
 		cmd.Main()
+		// PID 1 must never return — power off the VM
+		unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)
 	} else if cruntime.IsChild() {
 		cruntime.ContainerInitProc()
 	} else {
