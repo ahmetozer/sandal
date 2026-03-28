c0d1a6ab62372b571a205b246a34ed5276b13143
Ahmet OZER <10574489+ahmetozer@users.noreply.github.com>
2026-03-28 06:42:50 +0000

    fix: kernel panic at exit

--- main_linux.go
+++ main_linux.go
@@ -25,6 +25,13 @@ func platformMain() {
 			unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)
 			os.Exit(1)
 		}
+		// Override ExitHandler so cmd.Main() triggers a VM power-off instead
+		// of os.Exit(). The exit_linux.go init() may have missed SANDAL_VM_ARGS
+		// because importKernelCmdlineEnv() hadn't run yet at package init time.
+		cmd.ExitHandler = func(code int) {
+			time.Sleep(50 * time.Millisecond)
+			unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)
+		}
 		cmd.Main()
 		// Let the PL011 tty drain buffered output before powering off.
 		// The interrupt-driven TX needs CPU time to transmit remaining bytes.
