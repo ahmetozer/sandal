0ab9c2d29644ac72d75a00cb74027ca1033f52e9
Ahmet OZER <10574489+ahmetozer@users.noreply.github.com>
2026-04-06 23:01:58 +0000

    feat: env support for darwin

--- pkg/sandal/exec_darwin.go
+++ pkg/sandal/exec_darwin.go
@@ -8,6 +8,6 @@ import (
 	"github.com/ahmetozer/sandal/pkg/container/config"
 )
 
-func execNative(c *config.Config, args []string, user, dir string, tty bool) error {
+func execNative(c *config.Config, args []string, user, dir string, tty bool, extraEnv []string) error {
 	return fmt.Errorf("native container exec is not available on macOS")
 }
